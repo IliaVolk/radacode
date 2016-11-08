22c870c98b9593889cf1c9206c235c9737579fd2
IliaVolk <liailia119@gmail.com>
2016-11-08 16:09:51 +0200

    word mistake

--- gulpfile.js
+++ gulpfile.js
@@ -14,14 +14,18 @@ gulp.task('sass', function () {
         .pipe(concatCss("style.css"))
         .pipe(gulp.dest('./'));
 });
-
+gulp.task('index', function(){
+    return gulp.src('./app/index.html')
+        .pipe(gulp.dest("./"))
+})
 
 gulp.task('sass:watch', function () {
     gulp.watch('./app/**/*.sass', ['sass']);
+    gulp.watch('./app/index.html', ['index'])
 });
 
 gulp.task("clean", function(){
-    gulp.src(['style.css'])
+    gulp.src(['index.html', 'style.css'])
         .pipe(clean())
 })
-gulp.task('default', ['clean', 'sass','sass:watch'])
+gulp.task('default', ['clean', 'sass', 'index','sass:watch'])
